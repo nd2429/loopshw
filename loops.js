37c20f36d8044998606abfb565fcdb044fe3f32e
Nd2429 <nataliedom1987@gmail.com>
2022-11-23 14:19:17 -0500

    first commit

---
+++ loops.js
@@ -0,0 +1,37 @@
+
+
+//Write a for loop that logs 0-15;
+// for (let i= 0; i <=15; i++){
+//     console.log(i);
+// }
+
+// Write a for loop that counts from 15-0
+// for (let i= 15; i >=0; i--){
+//     console.log(i);
+
+// }
+
+// Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+
+// for (let laps= 0; laps <=2; laps++)
+// {
+// if (laps==1){
+
+//   console.log("thats another lap");
+ 
+
+// }
+//  else {
+
+//    console.log(laps);
+//  } 
+// }
+
+
+// git add, commit, push
+// Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+// Write a loop that logs all even numbers from 0-700.
+// Write a loop that logs all odd numbers from 0-700.
+// Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and log when things were invented. For example when i = 1902 the code should log “it is 1902 - the teddy bear is invented!”
+// git add, commit, push
+
