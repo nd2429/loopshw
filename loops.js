d192a198ef0625848ab2389432322310ae55fdc1
nd2429 <58461653+nd2429@users.noreply.github.com>
2022-11-23 20:15:30 -0500

    second commit

--- loops.js
+++ loops.js
@@ -1,17 +1,22 @@
 
 
-//Write a for loop that logs 0-15;
-// for (let i= 0; i <=15; i++){
+
+//1. Write a for loop that logs 0-15;
+// 
+
+//for (let i= 0; i <=15; i++){
 //     console.log(i);
 // }
 
-// Write a for loop that counts from 15-0
-// for (let i= 15; i >=0; i--){
+//2.  Write a for loop that counts from 15-0
+// 
+
+//for (let i= 15; i >=0; i--){
 //     console.log(i);
 
 // }
 
-// Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+// 3. Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
 
 // for (let laps= 0; laps <=2; laps++)
 // {
@@ -29,9 +34,48 @@
 
 
 // git add, commit, push
-// Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
-// Write a loop that logs all even numbers from 0-700.
-// Write a loop that logs all odd numbers from 0-700.
-// Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and log when things were invented. For example when i = 1902 the code should log “it is 1902 - the teddy bear is invented!”
+
+
+//4.  Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+
+
+
+
+
+//5. Write a loop that logs all even numbers from 0-700.
+// 
+
+// let m = 0, n = 700;
+// for (let i = m; i <= n; i++) {
+//   if (i % 2===0) 
+    
+//   console.log("even");
+
+//   else {
+
+//     console.log(i);
+//   }
+// }
+
+// console.log(i);
+
+
+
+// 6. Write a loop that logs all odd numbers from 0-700.
+// let m = 0, n = 700;
+//     for (let i = m; i <= n; i++) {
+//          if (i % 2===0) 
+    
+//    console.log(i);
+
+//         else {
+
+//             console.log("odd");
+//   }
+// }
+
+//         console.log(i);
+
+// 7. Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and log when things were invented. For example when i = 1902 the code should log “it is 1902 - the teddy bear is invented!”
 // git add, commit, push
 
